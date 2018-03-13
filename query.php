69d0fe665285057a502af13bab83a9c2caa99bb7
= <bernardoarodriguez@gmail.com>
2018-03-12 19:10:26 -0700

    fix curlopt parameter type

--- query.php
+++ query.php
@@ -1,7 +1,7 @@
 <?php
 $url = "https://www.random.org/integers/?num=3&min=0&max=255&col=1&format=plain&rnd=new";
 $ch = curl_init();
-curl_setopt($ch, curlopt_url, $url);
+curl_setopt($ch, constant(curlopt_url), $url);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
