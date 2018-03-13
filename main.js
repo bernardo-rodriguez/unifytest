d0b5dedef3c06c6b65b687a98183d3e7e8705347
= <bernardoarodriguez@gmail.com>
2018-03-12 20:58:59 -0700

    final version

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 $.get("query.php", function(data){
     alert("Data: " + data);
-    document.getElementById("php").innerHTML = data;
+    document.getElementById("php").innerHTML = "Retrieved data: " + data;
+    randomArray = parseNumbers(data);
+    var c = document.getElementById("bitmap");
+    var ctx = c.getContext("2d");
+    var img = ctx.createImageData(128, 128);
+    console.log(img.data.length);
+    for (j = 0; j < 350; j += 1) {
+    for (i = 0; i < 350; i += 4) {
+        img.data[i*j] = randomArray[i];
+        img.data[i*j+1] = randomArray[i+1];
+        img.data[i*j+2] = randomArray[i+2];
+        img.data[i*j+3] = 255;
+    }
+    }
+    ctx.putImageData(img, 10, 10);
 });
+
+var parseNumbers = function(randomString) {
+    randomArray = [];
+    var i = 0;
+    var count = 0;
+    var currNum = "";
+    while (i < 381){
+        if (randomString.charCodeAt(count) != 9){
+            while (randomString.charCodeAt(count) != 9) {
+                currNum += randomString[count];
+                count += 1;
+            }
+            randomArray[i] = parseInt(currNum);
+            currNum = "";
+            i += 1; 
+        } else {
+            count += 1;
+        }
+    }
+    console.log(randomArray);
+    return randomArray;
+}
--- query.php
+++ query.php
@@ -1,7 +1,7 @@
 <?php
-$url = "https://www.random.org/integers/?num=3&min=0&max=255&col=1&format=plain&rnd=new";
+$url = "https://www.random.org/integers/?num=384&min=0&col=128&max=255&format=plain&rnd=new&base=10";
 $ch = curl_init();
-curl_setopt($ch, constant(curlopt_url), $url);
+curl_setopt($ch, CURLOPT_URL, $url);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
